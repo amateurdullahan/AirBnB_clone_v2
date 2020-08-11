6623191a02b2aa7a239adfe4a4cfcdbbc866ba71
amateurdullahan <zabrak12@gmail.com>
2020-08-11 21:20:31 +0000

    6 partial

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128),
+                  nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    @property
+    def cities(self):
+        """list of city instances"""
+        city_list = []
+        city_all = models.storage.all(City)
+        for city in city_all.values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
