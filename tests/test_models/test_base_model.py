2ac6c7c763ae70fa40dbf94cf0e0f950ef4d3a59
Kelsie Merchant <eislek02@gmail.com>
2020-08-12 02:48:29 -0500

    Initial commit of unittests and update code to return dictionary when cls argument provided for all method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,8 +2,7 @@
 """ """
 from models.base_model import BaseModel
 import unittest
-import datetime
-from uuid import UUID
+from datetime import datetime
 import json
 import os
 
@@ -88,12 +87,12 @@ class test_basemodel(unittest.TestCase):
     def test_created_at(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.created_at), datetime.datetime)
+        self.assertEqual(type(new.created_at), datetime)
 
     def test_updated_at(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
+        self.assertEqual(type(new.updated_at), datetime)
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)
