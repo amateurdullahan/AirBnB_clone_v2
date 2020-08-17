15a355938f4cb5745a751151ba94d225fba9c5b7
amateurdullahan <zabrak12@gmail.com>
2020-08-17 21:31:35 +0000

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # task 0
-sudo apt-get install nginx
-mkdir -p /data/web_static/shared
-mkdir -p /data/web_static/current
-mkdir -p /data/web_static/releases/test
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/releases/test/
 echo "Wangxian OTP" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/current /data/web_static/releases/test
-chown -R ubuntu:ubuntu /data
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data
 sed '/^\tserver_name*/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 service nginx restart 
